8bacc744b1af082adbe0088ad34b2746da2957f4
rahim <154564602+rx7iiim@users.noreply.github.com>
2025-11-20 10:39:31 +0100

    registration closed component

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,33 +4,12 @@ import SignupForm from "@/components/signup-form";
 import Footer from "@/components/footer";
 import Jury from "@/components/jury";
 import About from "@/components/about";
+import RegistrationClosed from "@/components/RegistrationClosed";
 
 export default function Home() {
   return (
     <main className="w-full">
-      <section id="hero">
-        <Hero />
-      </section>
-
-      <section id="about">
-        <About />
-      </section>
-
-      <section id="agenda">
-        <Agenda />
-      </section>
-
-      <section id="jury">
-        <Jury />
-      </section>
-
-      <section id="signup-section">
-        <SignupForm />
-      </section>
-
-      <section id="footer">
-        <Footer />
-      </section>
+      <RegistrationClosed />
     </main>
   );
 }
